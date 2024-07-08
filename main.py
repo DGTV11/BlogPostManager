742232308fcaeaeb1e79b6c64612692b3e730ba6
warm-milk-foam <ingocodingbecauseyes@gmail.com>
2024-07-08 15:48:02 +0800

    womp

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 #TODO: Implement stuff here
 # Trying commit
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "It works"
+
+if __name__ == "__main__":
+    app.run()
