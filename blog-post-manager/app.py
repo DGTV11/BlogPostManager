42c154aec7b4bc39a733c5045af55018fefc1964
Daniel Wee <danielweedk@gmail.com>
2024-07-08 17:45:28 +0800

    Formatted codebase using Black

--- blog-post-manager/app.py
+++ blog-post-manager/app.py
@@ -7,6 +7,7 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 class BlogPost:
     def __init__(self):
         title = self.title
@@ -16,27 +17,30 @@ class BlogPost:
     def list_blog_posts():
         return glob(os.path.join(os.path.dirname(__file__), "blog-posts", "*", ""))
 
-#Stuff
-@app.route('/info')
+
+# Stuff
+@app.route("/info")
 def info():
     return render_template("info.html")
 
 
-@app.route('/posts/<string>', methods=["GET", "POST"])
+@app.route("/posts/<string>", methods=["GET", "POST"])
 def edit_post(post_name):
     if post_name not in BlogPost.list_blog_posts():
-        abort(404) #TODO: make error page more user-friendly
+        abort(404)  # TODO: make error page more user-friendly
     if request.method == "POST":
         title = request.form["title"]
-        content= request.form["content"]
+        content = request.form["content"]
         return
 
     return render_template("form.html")
 
+
 # Main
-@app.route('/')
+@app.route("/")
 def main():
     return render_template("index.html", post_names=BlogPost.list_blog_posts())
+
 
 if __name__ == "__main__":
     app.run()
