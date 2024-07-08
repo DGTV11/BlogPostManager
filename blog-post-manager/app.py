5de1eb2bae57d4efc6fb2d47ca34fb99bc9a6752
Daniel Wee <danielweedk@gmail.com>
2024-07-08 17:00:00 +0800

    Added CONTRIB_LOG.md and updated app.py

--- blog-post-manager/app.py
+++ blog-post-manager/app.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from markupsafe import escape
 
 app = Flask(__name__)
 
 def list_blog_posts():
-    return list(filter(lambda folder_name: not folder_name.startswith('_'), os.listdir(os.path.join(os.path.dirname(__file__), "blog-posts"))))
+    return list(filter(lambda folder_name: not (folder_name.startswith('_') or folder_name.startswith('.')), os.listdir(os.path.join(os.path.dirname(__file__), "blog-posts"))))
 
 @app.route('/info')
 def info():
@@ -14,14 +14,14 @@ def info():
 
 @app.route('/')
 def main():
+    return f"""<h1>Blog Post Manager</h1>
     
-    return """<h1>Blog Post Manager</h1>
-
     """
 
 @app.route('/posts/<string>')
 def edit_post(post_name):
-    pass
+    if post_name not in list_blog_posts():
+        abort(404) #TODO: make error page more user-friendly
 
 if __name__ == "__main__":
     app.run()
