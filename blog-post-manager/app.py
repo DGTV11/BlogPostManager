3c7ab0a6a58793affcc369bda14a68296b66f0ce
Daniel Wee <danielweedk@gmail.com>
2024-07-08 17:19:24 +0800

    Fixed another bug

--- blog-post-manager/app.py
+++ blog-post-manager/app.py
@@ -1,7 +1,7 @@
 from glob import glob
 import os
 
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
