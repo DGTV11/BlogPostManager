9074d396d81f750b708d1e7ef64089efb5617169
Daniel Wee <danielweedk@gmail.com>
2024-07-15 15:44:41 +0800

    Renamed form.html (bad name) to editor.html (more representative of its true function)

--- blog-post-manager/app.py
+++ blog-post-manager/app.py
@@ -8,12 +8,7 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-class BlogPost:  # the blog post object
-    def __init__(self, date, title, content):
-        date = self.date
-        title = self.title
-        content = self.content
-
+class BlogPostHelpers:
     @staticmethod
     def list_blog_posts():
         blog_post_filepaths = glob(
@@ -33,14 +28,14 @@ def info():
 
 @app.route("/posts/<postname>", methods=("GET", "POST"))
 def posts(postname):  # check GH Project for TODO list (to fix this)
-    if postname not in BlogPost.list_blog_posts():
+    if postname not in BlogPostHelpers.list_blog_posts():
         abort(404)  # TODO: make error page more user-friendly
 
     blog_post_folder_path = os.path.join(os.path.dirname(__file__), "blog-posts", postname)
     with open(os.path.join(blog_post_folder_path, "content.md"), "r") as f:
         postcontent = f.read()
 
-    return render_template("form.html", post_name=postname, post_content=postcontent)
+    return render_template("editor.html", post_name=postname, post_content=postcontent)
 
 
 # Main
@@ -68,7 +63,7 @@ def main():
                     os.path.join(os.path.dirname(__file__), "blog-posts", post_name)
                 )
 
-    return render_template("index.html", post_names=BlogPost.list_blog_posts())
+    return render_template("index.html", post_names=BlogPostHelpers.list_blog_posts())
 
 
 if __name__ == "__main__":
